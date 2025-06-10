838d1b6d3cec45e9d397729216e75e55528d892f
lordChaitanya <dataprivacy4588@gmail.com>
2025-06-10 06:55:57 +0530

    prisma is init

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,6 +2,26 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
-app.get('/', (c) => c.text('Hello from Hono + Cloudflare Workers!'));
+app.post('/api/v1/signup',(c)=>{
+    return c.text("")
+})
+
+app.post('/api/v1/signin',(c)=>{
+    return c.text("")
+})
+
+app.post('/api/v1/blog',(c)=>{
+    return c.text("")
+})
+
+app.put('/api/v1/blog',(c)=>{
+    return c.text("")
+})
+
+app.get('/api/v1/blog/:id',(c)=>{
+    return c.text("")
+})
+
+
 
 export default app;
